0b5ae3ce0b20e8b9bc187b373593742613f8f322
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-06-06 17:37:32 +0100

    Update run.py

--- run.py
+++ run.py
@@ -16,6 +16,11 @@ os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 tstart=time.time()
 
 torch.cuda.empty_cache()
+print('Total memory:',torch.cuda.get_device_properties(0).total_memory)
+print('Researved:',torch.cuda.memory_reserved(0))
+print('Allocated:',torch.cuda.memory_allocated(0))
+print('Free:',torch.cuda.memory_reserved(0)-torch.cuda.memory_allocated(0))
+print('\n\n')
 
 # Arguments
 
