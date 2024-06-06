f5c8f42629f3dc4df693a454249a288c5bb50643
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-06-06 19:53:47 +0100

    Update run.py

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 tstart=time.time()
 
 torch.cuda.empty_cache()
-# os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:<enter-size-here>"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:100"
 
 # Arguments
 
