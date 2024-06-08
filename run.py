9c5b9a7baf01a9865cc340e1add541886ee5e4cd
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-06-08 20:37:25 +0100

    Update run.py

--- run.py
+++ run.py
@@ -259,7 +259,7 @@ for t,ncla in taskcla:
         checkpoint_fisher={}
         for n,_ in appr.model.named_parameters():
             checkpoint_fisher[n]=appr.fisher[n].clone().cpu() ## Changes to make space on GPU: #9
-        checkpoint_fisher_old, checkpoint_fisher_for_loss = appr.fisher, appr.fisher_old, appr.fisher_for_loss
+        checkpoint_fisher_old, checkpoint_fisher_for_loss = appr.fisher_old, appr.fisher_for_loss
         for lamb_i,plot_lamb in enumerate(args.plot_lambs):
             for thres_i,plot_thres in enumerate([0.5]): #,0.6,0.7,0.8,0.9]):
                 print('\nTraining for',lamb_i,thres_i,'\n')
