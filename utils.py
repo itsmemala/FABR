2bf835804c587c44263291e443ff4c74db4c4576
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-07-11 15:49:08 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -360,6 +360,14 @@ def get_my_lambda(idrandom,t,class_counts):
     return my_lambda
 
 ########################################################################################################################
+class CPU_Unpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'torch.storage' and name == '_load_from_bytes':
+            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+        else:
+            return super().find_class(module, name)
+
+########################################################################################################################
 # vLA.1
 def modified_fisher(fisher,fisher_old
                     ,train_f1,best_index
