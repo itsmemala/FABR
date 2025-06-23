f42183e69b195a3e748f66846514f6573a0665d7
Malavika Suresh <itsmemala@users.noreply.github.com>
2025-06-23 22:52:59 +0100

    Update intent_sh_adabop_chsf.sh

--- intent_sh_adabop_chsf.sh
+++ intent_sh_adabop_chsf.sh
@@ -6,13 +6,14 @@
 note=$1 #random10
 randid=$2 #10
 seed=$3 #0
+svd_thres=$4
 dataset='hwu64'
 lr_array_t0=(0.00003 0.0003)
 lr_array=(0.00003 0.00003 0.00003 0.00003 0.0003 0.0003 0.0003 0.0003)
 tc_epsilon_array=(0.3 0.3 0.5 0.5 0.3 0.3 0.5 0.5)
 tc_lamb_s_array=(0.3 0.08 0.3 0.08 0.3 0.08 0.3 0.08)
 tc_lamb_l_array=(0.1 0.03 0.1 0.03 0.1 0.03 0.1 0.03)
-res_path="/home/local/data/ms/fabr_data/IntentSH/Adabop/${note}seed${seed}/IntentSH_Adabop_t"
+res_path="/home/local/data/ms/fabr_data/IntentSH/Adabop_svd${svd_thres}/${note}seed${seed}/IntentSH_Adabop_t"
 
 id=0
 printf "\n\nRunning search for task 0\n\n"
@@ -45,7 +46,7 @@ do
 		printf "\n\nLR Iteration $lr\n\n"
 		custom_lr="$past_lr,$lr"
 		mkdir -p  ${res_path}${id}_gold.${lr_id}/
-		python  FABR//run.py --bert_model 'bert-base-uncased' --experiment hwu64 --approach bert_adapter_adabop --backbone bert_adapter --baseline adabop --note $note --idrandom $randid --seed $seed --scenario cil --use_rbs True --train_batch_size 128 --num_train_epochs 30 --eval_batch_size 128 --valid_loss_es 0.002 --lr_patience 3 --custom_lr $custom_lr --tc_epsilon $tc_epsilon --tc_lamb_s $tc_lamb_s --tc_lamb_l $tc_lamb_l --break_after_task $id --my_save_path ${res_path}${id}_gold.${lr_id}/ --start_at_task $id --start_model_path $start_model_path
+		python  FABR//run.py --bert_model 'bert-base-uncased' --experiment hwu64 --approach bert_adapter_adabop --backbone bert_adapter --baseline adabop --note $note --idrandom $randid --seed $seed --scenario cil --use_rbs True --train_batch_size 128 --num_train_epochs 30 --eval_batch_size 128 --valid_loss_es 0.002 --lr_patience 3 --custom_lr $custom_lr --tc_epsilon $tc_epsilon --tc_lamb_s $tc_lamb_s --tc_lamb_l $tc_lamb_l --break_after_task $id --my_save_path ${res_path}${id}_gold.${lr_id}/ --start_at_task $id --start_model_path $start_model_path --svd_thres $svd_thres
 	done
 	
 	python3 FABR/return_best_lr.py --my_save_path ${res_path}${id}_gold --rand_idx $randid --seed $seed --dataset $dataset --max_lr_id $lr_id --tid $id
