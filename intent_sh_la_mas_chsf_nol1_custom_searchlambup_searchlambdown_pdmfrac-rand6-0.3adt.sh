e87a8f7f2c520811b95bd060b4eb57c060e9f89c
Malavika Suresh <itsmemala@users.noreply.github.com>
2025-08-05 11:52:40 +0100

    s

--- intent_sh_la_mas_chsf_nol1_custom_searchlambup_searchlambdown_pdmfrac-rand6-0.3adt.sh
+++ intent_sh_la_mas_chsf_nol1_custom_searchlambup_searchlambdown_pdmfrac-rand6-0.3adt.sh
@@ -20,7 +20,7 @@ growth=0.8
 res_path="/home/local/data/ms/fabr_data/IntentSH/IntentSH_LAMAS_NoL1_Custom_ss_${pdm_frac}pdmfrac${no_frel_cut_max}/${note}seed${seed}_${acc_drop_threshold}adt/IntentSH_LAMAS_t"
 
 past_lr=0.0003,0.0003,0.00003,0.00003,0.00003,0.00003
-past_lamb=0,28.24295365,28.2429537,0.229376,0.09395241
+past_lamb=0,28.24295365,28.2429537,0.229376,0.09395241,0.09395241
 best_lamb=0.09395241
 start_model_path="${res_path}4.5.LA_phase.27/"
 
@@ -28,42 +28,15 @@ id_array=(5)
 for id in "${id_array[@]}"
 do
 	best_lr_id=1
-
-	## Lamb
-	lamb=0.09395241
-	lamb_i=0
-	found_best=false
-	while [ $found_best=false ]
-	do
-		((lamb_i++))
-		custom_lr=$past_lr
-		custom_lamb="$past_lamb,$lamb"
-		printf "\n\nLamb Iteration $custom_lamb \n\n"
-		mkdir -p  ${res_path}${id}.${lamb_i}/
-		python  FABR//run.py --bert_model 'bert-base-uncased' --experiment hwu64 --approach bert_adapter_ewc_freeze --imp function --baseline ewc_freeze --backbone bert_adapter --note $note --idrandom $randid --seed $seed --scenario cil --use_rbs True --train_batch_size 128 --num_train_epochs 50 --valid_loss_es 0.002 --lr_patience 5 --custom_lr $custom_lr --custom_lamb $custom_lamb --fisher_combine max --break_after_task $id --save_alpharel True --my_save_path ${res_path}${id}.${lamb_i}/ --start_at_task $id --start_model_path $start_model_path --only_mcl True
-		python3 FABR/calc_next_lamb.py --my_save_path ${res_path}${id} --rand_idx $randid --seed $seed --dataset $dataset --lamb_i $lamb_i --lamb $lamb --decay $decay --acc_drop_threshold $acc_drop_threshold --tid $id
-		found_best=`cat ${res_path}${id}.${lamb_i}_foundbestlamb.txt`
-		python3 FABR/plot_lamb_results.py --my_save_path ${res_path}${id} --rand_idx $randid --seed $seed --dataset $dataset --lamb_i $lamb_i --lamb $lamb --acc_drop_threshold $acc_drop_threshold --tid $id
-		if [ $found_best = found ]; then
-			best_lamb=$lamb
-			best_lamb_i=$lamb_i
-			break
-		fi
-		lamb=`cat ${res_path}${id}_next_lamb.txt`
-	done
-	
-	past_lamb="$past_lamb,$best_lamb"
-	
-	# if [ "$id" -eq 1 ]; then
-		# elasticity_up_max_lamb=`cat ${res_path}${id}_min_lamb_w_newtask_zero.txt`
-	# fi
+	best_lamb=0.09395241
+	best_lamb_i=1
 	
 	la_model_path="${res_path}${id}.${best_lamb_i}.LA_phase.1/"
 	
 	## Lamb Down
-	lamb_down=1.0
-	elasticity_up_mult=1.0
-	alpha_lamb_i=0
+	lamb_down=0.0000008
+	elasticity_up_mult=0.000005
+	alpha_lamb_i=3
 	found_best=false
 	while [ $found_best=false ]
 	do
